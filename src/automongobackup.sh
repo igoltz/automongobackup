4746d9b34c18b0555f8669d5e1d6c0bc5a6df8a4
Goltz, Immo <immo.goltz@goethe.de>
2021-02-26 09:14:02 +0100

    Don't overwrite env. settings for DBHOST, DBPORT, BACKUPDIR with default values

--- src/automongobackup.sh
+++ src/automongobackup.sh
@@ -56,13 +56,13 @@ set -eo pipefail
 # DBAUTHDB=""
 
 # Host name (or IP address) of mongo server e.g localhost
-DBHOST="127.0.0.1"
+DBHOST="${DBHOST:-127.0.0.1}"
 
 # Port that mongo is listening on
-DBPORT="27017"
+DBPORT="${DBPORT:-27017}"
 
 # Backup directory location e.g /backups
-BACKUPDIR="/var/backups/mongodb"
+BACKUPDIR="${BACKUPDIR:-/var/backups/mongodb}"
 
 # Mail setup
 # What would you like to be mailed to you?
